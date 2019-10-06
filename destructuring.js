3b53d9438a38287b3a0cca1d50705f2c8c410105
Abe Choi <abraham.choi@icloud.com>
2019-10-05 22:53:02 -0700

    Destructuring

--- destructuring.js
+++ destructuring.js
@@ -1,9 +1,13 @@
 /*
-        FREE CODE CAMP - ES6: Use Destructuring Assignment to Assign Variables from Objects
-        Examples of destructuring assignments...
+        DESTRUCTURING - Examples of destructuring assignments
+        
+        1. Destructuring assignments from an object
+        2. Destructuring assignments from a nested object
+        3. Destructuring assignments from arrays 
+        4. Destructuring assignments with rest operators
  */
 
- // Destructuring assignments from an object
+ // 1. Destructuring assignments from an object
 const AVG_TEMPERATURES = { today: 77.5,tomorrow: 79};
 
         function getTempOfTmrw(avgTemperatures) {
@@ -18,7 +22,7 @@ const AVG_TEMPERATURES = { today: 77.5,tomorrow: 79};
         console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
 
 
-// Destructuring assignments from a nested object
+// 2. Destructuring assignments from a nested object
 const LOCAL_FORECAST = {
     today: { min: 72, max: 83 },
     tomorrow: { min: 73.3, max: 84.6 }
@@ -34,8 +38,8 @@ const LOCAL_FORECAST = {
   
   console.log(getMaxOfTmrw(LOCAL_FORECAST)); // should be 84.6
 
-  // Destructuring assignments from arrays
-  let a = 8, b = 6;
+// 3. Destructuring assignments from arrays
+let a = 8, b = 6;
 (() => {
   "use strict";
   // change code below this line
@@ -44,3 +48,16 @@ const LOCAL_FORECAST = {
 })();
 console.log(a); // should be 6
 console.log(b); // should be 8
+
+// 4. Destructuring assignments with rest operators
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  "use strict";
+  // change code below this line
+  const [a,b, ...arr] = list; // change this
+  // change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
